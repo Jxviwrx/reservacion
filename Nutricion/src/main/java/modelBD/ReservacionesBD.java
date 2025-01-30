b716c2a5aacbf3b69feba353ba2d7ed38da1bf47
ewinFanto <iasc19668@isic.edu.mx>
2025-01-29 19:21:22 -0700

    reservacionesbd

---
+++ Nutricion/src/main/java/modelBD/ReservacionesBD.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelBD;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Usuario
+ */
+@Entity
+@Table(name = "reservaciones")
+public class ReservacionesBD {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    String nombre_paciente;
+    String direccion_paciente;
+    String telefono_paciente;
+    String correo_electronico;
+    String fecha;
+    String hora;
+    String motivo_consulta;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre_paciente() {
+        return nombre_paciente;
+    }
+
+    public void setNombre_paciente(String nombre_paciente) {
+        this.nombre_paciente = nombre_paciente;
+    }
+
+    public String getDireccion_paciente() {
+        return direccion_paciente;
+    }
+
+    public void setDireccion_paciente(String direccion_paciente) {
+        this.direccion_paciente = direccion_paciente;
+    }
+
+    public String getTelefono_paciente() {
+        return telefono_paciente;
+    }
+
+    public void setTelefono_paciente(String telefono_paciente) {
+        this.telefono_paciente = telefono_paciente;
+    }
+
+    public String getCorreo_electronico() {
+        return correo_electronico;
+    }
+
+    public void setCorreo_electronico(String correo_electronico) {
+        this.correo_electronico = correo_electronico;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getMotivo_consulta() {
+        return motivo_consulta;
+    }
+
+    public void setMotivo_consulta(String motivo_consulta) {
+        this.motivo_consulta = motivo_consulta;
+    }
+    
+    
+    
+}
