552d130572600e31315b42ad09fcadf5e2f8b45b
Jxvixrcx <shychi.csz@gmail.com>
2025-02-12 09:17:14 -0700

    Se agrego el faltante

--- Nutricion/src/main/java/Service/Usuarios.java
+++ Nutricion/src/main/java/Service/Usuarios.java
@@ -4,6 +4,7 @@
  */
 package Service;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
@@ -20,6 +21,8 @@ import org.hibernate.cfg.Configuration;
 
 import java.util.Date;
 import java.util.Calendar;
+import javax.ws.rs.GET;
+import javax.ws.rs.QueryParam;
 
 @Path("Usuarios")
 public class Usuarios {
@@ -66,16 +69,14 @@ public class Usuarios {
                     .build();
         }
 
-        // Asignamos "1" a activo independientemente del valor recibido
         activo = 1; // Establecer siempre el valor de 'activo' como 1
 
-        // Obtener la fecha actual solo con el año, mes y día
         Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, 0);   // Establecer la hora a medianoche
-        calendar.set(Calendar.MINUTE, 0);        // Establecer los minutos a 0
-        calendar.set(Calendar.SECOND, 0);        // Establecer los segundos a 0
-        calendar.set(Calendar.MILLISECOND, 0);   // Establecer los milisegundos a 0
-        Date fecha_ingreso = calendar.getTime();  // Obtiene la fecha sin la hora
+        calendar.set(Calendar.HOUR_OF_DAY, 0);   
+        calendar.set(Calendar.MINUTE, 0);        
+        calendar.set(Calendar.SECOND, 0);        
+        calendar.set(Calendar.MILLISECOND, 0);   
+        Date fecha_ingreso = calendar.getTime();  
 
         Session session = null;
         Transaction transaction = null;
@@ -90,7 +91,7 @@ public class Usuarios {
             usuario.setCorreo_electronico(correo_electronico);
             usuario.setContrasena(contrasena);
             usuario.setRol_usuario(rol_usuario);
-            usuario.setActivo(activo == 1);  // Esto siempre será verdadero, ya que 'activo' es 1
+            usuario.setActivo(activo == 1);  
             usuario.setFecha_ingreso(fecha_ingreso);  // Asignar la fecha de ingreso sin la hora
 
             // Guardar el usuario en la base de datos
@@ -107,6 +108,169 @@ public class Usuarios {
                     .build();
         } finally {
             session.close();
+        }
+    }
+
+    @GET
+    @Path("eliminar")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response eliminar(@QueryParam("id") Long id) {
+        Session session = null;
+        Transaction transaction = null;
+
+        if (id == null || id <= 0) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("{\"error\":\"El campo 'id' es obligatorio y debe ser mayor a cero.\"}")
+                    .build();
+        }
+
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+
+            UsuariosBD usuario = session.get(UsuariosBD.class, id);
+
+            if (usuario == null) {
+                return Response.status(Response.Status.NOT_FOUND)
+                        .entity("{\"error\":\"Usuario no encontrado con id: " + id + "\"}")
+                        .build();
+            }
+
+            usuario.setActivo(false);
+            session.update(usuario);
+            transaction.commit();
+
+            return Response.ok("{\"message\":\"Usuario desactivado exitosamente\"}")
+                    .build();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("{\"error\":\"Error al desactivar al usuario: " + e.getMessage() + "\"}")
+                    .build();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+    @POST
+   @Path("editar")
+   @Produces(MediaType.APPLICATION_JSON)
+   public Response editar(@FormParam("id") long id,
+           @FormParam("nombre") String nombre,
+           @FormParam("direccion") String direccion,
+           @FormParam("correo_electronico") String correo_electronico,
+           @FormParam("contrasena") String contrasena,
+           @FormParam("rol_usuario") String rol_usuario) {
+
+      Session session = null;
+      Transaction transaction = null;
+
+      if (id <= 0) {
+         return Response.status(Response.Status.BAD_REQUEST)
+                 .entity("{\"error\":\"El campo 'id' es obligatorio y debe ser mayor a cero.\"}")
+                 .build();
+      }
+
+      try {
+
+         session = sessionFactory.openSession();
+         transaction = session.beginTransaction();
+
+         UsuariosBD usuario = session.get(UsuariosBD.class, id);
+         if (usuario == null) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("{\"error\":\"Usuario no encontrado.\"}")
+                    .build();
+         }
+
+         if (nombre != null && !nombre.trim().isEmpty()) {
+            usuario.setNombre(nombre);
+         }
+         
+         if (direccion != null && !direccion.trim().isEmpty()) {
+            usuario.setDireccion(direccion);
+         }
+         
+         if (correo_electronico != null && !correo_electronico.trim().isEmpty()) {
+            usuario.setCorreo_electronico(correo_electronico);
+         }
+         
+         if (contrasena != null && !contrasena.trim().isEmpty()) {
+            usuario.setContrasena(contrasena);
+         }
+         
+         if (rol_usuario != null && !rol_usuario.trim().isEmpty()) {
+            usuario.setRol_usuario(rol_usuario);
+         }
+         
+
+         session.update(usuario);
+         transaction.commit();
+
+         return Response.ok("{\"message\":\"Usuario actualizado exitosamente\"}")
+                 .build();
+      } catch (Exception e) {
+         if (transaction != null) {
+            transaction.rollback();
+         }
+         e.printStackTrace();
+         return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                 .entity("{\"error\":\"Error al actualizar el Usuario: " + e.getMessage() + "\"}")
+                 .build();
+      } finally {
+         if (session != null) {
+            session.close();
+         }
+      }
+   }
+
+   @GET
+    @Path("activar")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response activar(@QueryParam("id") Long id) {
+        Session session = null;
+        Transaction transaction = null;
+
+        if (id == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("{\"error\":\"El campo 'id' es obligatorio..\"}")
+                    .build();
+        }
+
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+
+            UsuariosBD usuario = session.get(UsuariosBD.class, id);
+
+            if (usuario == null) {
+                return Response.status(Response.Status.NOT_FOUND)
+                        .entity("{\"error\":\"Usuario no encontrado con id: " + id + "\"}")
+                        .build();
+            }
+
+            usuario.setActivo(true);
+            session.update(usuario);
+            transaction.commit();
+
+            return Response.ok("{\"message\":\"Usuario reactivado exitosamente\"}")
+                    .build();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("{\"error\":\"Error al desactivar al usuario: " + e.getMessage() + "\"}")
+                    .build();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
         }
     }
 }
